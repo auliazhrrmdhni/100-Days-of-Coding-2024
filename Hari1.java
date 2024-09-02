0578b24c691b2b697027ceb51c29ef03df21175a
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-02 17:19:10 +0800

    Create Hari1.java
    Biodata

---
+++ Hari1.java
@@ -0,0 +1,13 @@
+package ISC100Hari;
+
+public class Hari1 {
+    public static void main(String[] args) {
+        System.out.println("Nama: Aulia Zahra Ramadhani");
+        System.out.println("NIM: D0222346");
+        System.out.println("Angkatan: 22");
+        System.out.println("Asal: Luwu Timur, Sulawesi Selatan");
+        System.out.println("Tempat, tanggal lahir: Majene, 28-10-2004");
+    
+    }
+    
+}
