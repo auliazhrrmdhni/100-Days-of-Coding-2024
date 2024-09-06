b9e21841d38083dddb1172c449b6a6c937076608
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-06 09:03:46 +0800

    Create Hari5.java
    Penggunaan komentar

---
+++ Hari5.java
@@ -0,0 +1,14 @@
+package ISC100Hari;
+
+public class Day5 {
+    public static void main(String[] args) {
+        
+        //System.out.println("Halo");
+        
+        /*for (int i = 0; i < 10; i++){
+            System.out.print(i);
+        }
+        */
+        System.out.println("Keduanya adalah komentar");
+    }
+}
