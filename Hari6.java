115acbfd6fccc4802a206f3eba10ae94361e2328
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-07 15:04:05 +0800

    Create Hari6.java
    Deklarasi variabel untuk masing-masing tipe data

---
+++ Hari6.java
@@ -0,0 +1,24 @@
+package ISC100Hari;
+
+public class Day6 {
+    public static void main(String[] args) {
+        
+        //Mendeklarasikan variabel beberapa tipe data
+        String kata = "Kerja";
+        int numb = 23;
+        double desimal = 3.14;
+        float desimalJuga = 3.14f;
+        int umur = 17;
+        boolean buatKTP = umur>=17;
+        
+        //Jika ingin melakukan pemanggilan pada variabel yang telah dibuat
+        System.out.println(kata);
+        System.out.println(numb);
+        System.out.println(desimal);
+        System.out.println(desimalJuga);
+        
+        System.out.println("Bisa buat KTP: " +buatKTP);
+        
+    }
+    
+}
