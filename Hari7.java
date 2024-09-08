debaf95f2c03ee193ba895ee9d49f7af6aac98d8
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-08 13:26:32 +0800

    Create Hari7.java
    Inisialisasi variabel

---
+++ Hari7.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day7 {
+    public static void main(String[] args){
+        
+        int number = 28; //Menginisialisasi variabel number dengan nilai 28
+        String nama = "Aulia"; //Menginisialisasi variabel nama dengan nilai "Aulia"
+        double desimal = 23.4; //Menginisialisasi variabel desimal dengan nilai 23.4
+        
+    }
+}
