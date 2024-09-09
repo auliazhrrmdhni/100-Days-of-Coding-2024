d06e1ee45faf56cf2208d8b2a10841b27128c2fa
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-09 20:08:45 +0800

    Create Hari8.java
    Update nilai dari variabel

---
+++ Hari8.java
@@ -0,0 +1,13 @@
+package ISC100Hari;
+
+public class Day8 {
+    public static void main(String[] args){
+        
+        int number = 23; 
+        System.out.println("Nilai variabel number sebelum diupdate adalah " +number);
+        
+        number = 28; //mengupdate nilai variabel number menjadi 28
+        System.out.println("Nilai variabel number setelah diupdate adalah " +number);
+        
+    }
+}
