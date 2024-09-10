354d91b2990aaddf77b03bbd423164e69600317c
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-10 20:15:37 +0800

    Create Hari9.java
    variabel final

---
+++ Hari9.java
@@ -0,0 +1,12 @@
+package ISC100Hari;
+
+public class Day9 {
+    public static void main(String[] args){
+        
+        final int tanggal = 2304; //final variabel
+        /* tanggal = 2810; jika nilai variabel diubah seperti ini maka akan
+           error karena variabel tanggal adalah final
+        */
+        System.out.println(tanggal);
+    }
+}
