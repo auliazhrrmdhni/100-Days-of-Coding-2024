e856f87d3e41194c04c1bea9d31a138fe6b78a12
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-11 16:41:12 +0800

    Create Hari10.java
    tipe data byte

---
+++ Hari10.java
@@ -0,0 +1,10 @@
+package ISC100Hari;
+
+public class Day10 {
+    public static void main(String[] args) {
+        byte angka = -128;
+        angka = 127;
+        
+        System.out.println(angka);
+    }
+}
