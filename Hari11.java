3201772d07e82567e2c355d3131541b0e7c23343
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-12 19:40:43 +0800

    Create Hari11.java
    tipe data short

---
+++ Hari11.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day11 {
+    public static void main(String[] args) {
+        short angka = 417;
+        angka = 1034;
+        
+        System.out.println(angka);
+    }
+    
+}
