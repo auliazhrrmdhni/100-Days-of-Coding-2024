ab81509894a1a09b8351268ec644f9ea3b93dbae
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-13 19:24:46 +0800

    Create Hari12.java
    tipe data int

---
+++ Hari12.java
@@ -0,0 +1,12 @@
+package ISC100Hari;
+
+public class Day12 {
+    public static void main(String[] args) {
+        
+        int sisi = 8;
+        
+        int luasPersegi = sisi*sisi;
+        System.out.println("Luas persegi adalah: "+ luasPersegi);
+        
+    }
+}
