8a7ebf71579153299ac656cd7c6cfa1b798a4ec0
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-15 13:17:00 +0800

    Create Hari14.java
    Tipe data long

---
+++ Hari14.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day14 {
+    public static void main(String[] args){
+        
+        long numb = 12456;
+        long nomor = 10;
+        
+        System.out.println(nomor*numb);
+    }
+}
