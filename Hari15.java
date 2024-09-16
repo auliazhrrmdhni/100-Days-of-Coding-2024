6f467bc225128bd67fdf9315a21a03c7aad65a17
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-16 19:23:46 +0800

    Create Hari15.java
    Tipe data double

---
+++ Hari15.java
@@ -0,0 +1,10 @@
+package ISC100Hari;
+
+public class Day15 {
+    public static void main(String[] args){
+        int r = 4;
+        double pi = 3.14159265359;
+        double kelilingLingkaran = 2*pi*r;
+        System.out.println("Keliling lingkaran: "+kelilingLingkaran);
+    }
+}
