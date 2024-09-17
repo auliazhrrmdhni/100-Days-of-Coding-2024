070c379a269835cd4bba1164e22c01754e5ee957
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-17 19:34:13 +0800

    tipe data float

---
+++ Hari16.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day16 {
+    public static void main(String[] args){
+        float a = 4.5780f;
+        float z = 3.50024f;
+        double k = 2.480391864;
+        
+        System.out.println(a*z-k);
+    }
+}
