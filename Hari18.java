908eb678942194e8be7c5bbef7d7d548b55d1f7a
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-19 19:35:36 +0800

    Tipe data boolean

---
+++ Hari18.java
@@ -0,0 +1,13 @@
+package ISC100Hari;
+
+public class Day18 {
+    public static void main(String[] args){
+        boolean password = false;
+        if (password){
+            System.out.println("Password benar");
+        }
+        else{
+            System.out.println("Password salah");
+        }
+    }
+}
