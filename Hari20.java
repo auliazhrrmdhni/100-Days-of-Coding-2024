066ba41c0fa2524b690be1e404980046609d8580
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-21 20:08:31 +0800

    Tpe data reference: Byte

---
+++ Hari20.java
@@ -0,0 +1,12 @@
+package ISC100Hari;
+
+import java.util.Arrays;
+
+public class Day20 {
+    public static void main(String[] args){
+        Byte[] ganjil = {13, 15, 17};
+        Byte[] angka = ganjil;
+        
+        System.out.println(Arrays.toString(angka));
+    }
+}
