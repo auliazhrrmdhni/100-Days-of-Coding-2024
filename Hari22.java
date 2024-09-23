7d2e0eb9b100b51cf4cd4748d3a199d7e77556aa
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-23 20:40:21 +0800

    Tipe data reference: Integer

---
+++ Hari22.java
@@ -0,0 +1,16 @@
+package ISC100Hari;
+
+import java.util.ArrayList;
+
+public class Day22 {
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<Integer>();
+        
+        list.add(20);
+        list.add(4);
+        list.add(21);
+        
+        System.out.println("Arryalist: "+list);
+        
+    }
+}
