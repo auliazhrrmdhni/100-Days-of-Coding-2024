d88ac1ad9fccbcff9fb4e2c96a907ac9090c28aa
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-24 16:59:46 +0800

    Tipe data reference: Long

---
+++ Hari23.java
@@ -0,0 +1,16 @@
+package ISC100Hari;
+
+import java.util.ArrayList;
+
+public class Day23 {
+    public static void main(String[] args) {
+        
+        ArrayList<Long> num = new ArrayList<Long>();
+        
+        num.add(23L);
+        num.add(1L);
+        num.add(25L);
+        
+        System.out.println("Isi variabel num: "+num);
+    }
+}
