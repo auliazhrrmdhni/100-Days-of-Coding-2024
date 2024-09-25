4c5136e1c2751721e78c7ebbfb1fd49a2b88078d
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-25 09:58:06 +0800

    Tipe data reference: Float

---
+++ Hari24.java
@@ -0,0 +1,17 @@
+package ISC100Hari;
+
+import java.util.ArrayList;
+
+public class Day24 {
+    public static void main(String[] args){
+        
+        ArrayList<Float> angka = new ArrayList<Float>();
+        
+        angka.add(23.4f);
+        angka.add(10.28f);
+        angka.add(28.12f);
+        angka.add(14.2f);
+        
+        System.out.println("Isi variabel angka: "+angka);
+    }
+}
