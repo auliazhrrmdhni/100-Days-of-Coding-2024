0509ae73d74bbe392bcfa602f36885d3aa0c1e48
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-26 15:44:02 +0800

    Tipe data reference: Double

---
+++ Hari25.java
@@ -0,0 +1,17 @@
+package ISC100Hari;
+
+import java.util.ArrayList;
+
+public class Day25 {
+    public static void main(String[] args){
+        ArrayList<Double> number = new ArrayList<Double>();
+        
+        number.add(23.4);
+        number.add(10.28);
+        number.add(28.12);
+        number.add(14.2);
+        
+        System.out.println("Isi variabel number: "+number);
+    }
+    
+}
