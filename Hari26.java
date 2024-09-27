0729e61b69a0ac2d02a9d8b2fc3c6841b049e580
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-27 17:01:29 +0800

    Tipe data reference: Character

---
+++ Hari26.java
@@ -0,0 +1,17 @@
+package ISC100Hari;
+
+import java.util.ArrayList;
+
+public class Day26 {
+    public static void main(String[] args){
+        
+        ArrayList<Character> inisial = new ArrayList<Character>();
+        
+        inisial.add('A');
+        inisial.add('Z');
+        inisial.add('R');
+        
+        System.out.println("Inisial nama: "+inisial);
+        
+    }
+}
