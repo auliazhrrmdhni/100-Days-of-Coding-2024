598eec35c9812aa6fe7d6083dc1aa3f9085258b2
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-29 14:40:01 +0800

    Tipe data reference: Boolean

---
+++ Hari28.java
@@ -0,0 +1,13 @@
+package ISC100Hari;
+
+public class Day28 {
+    public static void main(String[] args){
+        Boolean hujan = true;
+        
+        if(hujan){
+            System.out.println("Bawa jas hujan");
+        }else{
+            System.out.println("kehujanan");
+        }
+    }
+}
