cf990f59eba87783ba38f6ab70970e453042a526
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-09-30 19:09:56 +0800

    Tipe data reference: BigInteger

---
+++ Hari29.java
@@ -0,0 +1,11 @@
+berikan penjelasan tentang kode ini package ISC100Hari;
+
+import java.math.BigInteger;
+
+public class Day29 {
+    public static void main(String[] args) {
+        BigInteger angka = new BigInteger("12345678987654321");
+        
+        System.out.println("Ini adalah contoh Big Integer:"+angka);
+    }
+}  
