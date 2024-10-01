db9b21ab24ee88180f792f7126c408959ba0aa9d
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-01 20:17:03 +0800

    Tipe data reference: BigDecimal

---
+++ Hari30.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+import java.math.BigInteger;
+
+public class Day29 {
+    public static void main(String[] args) {
+        BigInteger angka = new BigInteger("012345678987654321");
+        
+        System.out.println("Ini adalah contoh Big Integer:"+angka);
+    }
+}
