3e46ff51c4dffd6a7178b952640cfe40579f070a
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-02 16:10:14 +0800

    Konversi String ke tipe data primitif

---
+++ Hari31.java
@@ -0,0 +1,15 @@
+package ISC100Hari;
+
+public class Day31 {
+    public static void main(String[] args) {
+        
+        String bilBulat = "2304";
+        int bilangan = Integer.parseInt(bilBulat);
+        
+        String pecahan = "10.28";
+        double desimal = Double.parseDouble(pecahan);
+        
+        System.out.println(bilangan);
+        System.out.println(desimal);
+    }
+}
