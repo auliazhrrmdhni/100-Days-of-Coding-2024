4791f3cfbe1830e265c025e0f61a463508e61c39
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-03 19:10:16 +0800

    Konversi data primitif ke String

---
+++ Hari32.java
@@ -0,0 +1,15 @@
+package ISC100Hari;
+
+public class Day32 {
+    public static void main(String[] args) {
+        
+        int nilai = 423;
+        String angka = Integer.toString(nilai);
+        
+        double pecahan = 10.28;
+        String desimal = Double.toString(pecahan);
+        
+        System.out.println(nilai);
+        System.out.println(desimal);
+    }
+}
