8a615e04fd16bd8a259d0541c6e30fddf6020eff
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-04 19:45:53 +0800

    Type casting

---
+++ Hari33.java
@@ -0,0 +1,13 @@
+package ISC100Hari;
+
+public class Day33 {
+    public static void main(String[] args) {
+        
+        int nilai = 24;
+        System.out.println("Nilai integer: "+nilai);
+        
+        double desimal = nilai;
+        System.out.println("Nilai double "+desimal);
+      
+    }
+}
