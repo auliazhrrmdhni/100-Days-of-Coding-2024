350856335416354b7ecf215d02158eacf51ea6df
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-05 15:07:13 +0800

    Input dari keyboard

---
+++ Hari34.java
@@ -0,0 +1,14 @@
+package ISC100Hari;
+
+import java.util.Scanner;
+
+public class Day34 {
+    public static void main(String[] args) {
+        
+        Scanner  input = new Scanner(System.in);
+        
+        System.out.println("Nama: ");
+        String nama = input.nextLine();
+        System.out.println("Nama saya adalah: "+nama);
+    }
+}
