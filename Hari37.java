c70420d0da9e1001eb7294d82fceb467a3f67655
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-08 21:48:51 +0800

    Operator penugasan: pengisian nilai, pengisian dan penambahan, pengisian dan pengurangan

--- Hari37.java
+++ Hari37.java
@@ -1,26 +1,14 @@
 package ISC100Hari;
 
-import java.util.Scanner;
-
 public class Day37 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    
-        System.out.print("Angka1: ");
-        int angka1 = scanner.nextInt();
-        System.out.print("Angka2: ");
-        int angka2 = scanner.nextInt();
         
-        angka1+=angka2;
-        System.out.println("Hasil penjumlahan keduanya adalah "+angka1);
-        
-        System.out.print("Nilai1: ");
-        int nilai1 = scanner.nextInt();
-        System.out.print("Nilai2: ");
-        int nilai2 = scanner.nextInt();
-        
-        nilai1-=nilai2;
-        System.out.println("Hasil kurang dari keduanya adalah "+nilai1);        
+        int a = 2;
+        System.out.println("Nilai dari variabel a: "+a);
+        a+=8;
+        System.out.println("Nilai dari variabel a setelah ditambahkan: "+a);
+        a-=4;
+        System.out.println("Nilai dari variabel a setelah dikurang: "+a);
     }
     
-}
+} 
