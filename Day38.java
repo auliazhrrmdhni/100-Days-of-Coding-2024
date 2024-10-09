d94882b38236bcab27dbe1b9ec4c640122117118
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-09 13:54:04 +0800

    Operator penugasan: pengisian dan perkalian, pengisian dan pembagian

---
+++ Day38.java
@@ -0,0 +1,16 @@
+package ISC100Hari;
+
+public class Day38 {
+    public static void main(String[] args) {
+        
+        int a = 8;
+        int b = 4;
+        System.out.printf("Nilai dari variabel a adalah %d dan variabel b adalah %d%n", a, b);
+        
+        a*=b;
+        System.out.println("Hasil kali dari kedua variabel tersebut adalah: "+a);
+        
+        a/=b;
+        System.out.println("Hasil bagi dari kedua variabel tersebut adalah: "+a);
+    }
+}
