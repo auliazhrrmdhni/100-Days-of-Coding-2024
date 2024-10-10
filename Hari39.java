d125e8536559295dbce576de4cf76453191a1db6
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-10 18:43:29 +0800

    Operator penugasan: pengisian dan modulo

---
+++ Hari39.java
@@ -0,0 +1,12 @@
+package ISC100Hari;
+
+public class Day39 {
+    public static void main(String[] args) {
+        
+        int angka = 57;
+        
+        angka%=5;
+        System.out.println("Sisa bagi dari keduanya adalah "+angka);
+        
+    }
+}
