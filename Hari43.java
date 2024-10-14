13d3882cb2e58d8ac4b946cd79579d027a2592a9
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-14 18:36:02 +0800

    Operator perbandingan: Lebih besar sama dengan dan lebih kecil sama dengan

---
+++ Hari43.java
@@ -0,0 +1,16 @@
+package ISC100Hari;
+
+public class Day43 {
+    public static void main(String[] args) {
+        
+        byte a = 4;
+        byte b = 9;
+        byte c = 3*3;
+        
+        boolean nilai1 = a >= b;
+        boolean nilai2 = c <= b;
+        
+        System.out.println("Nilai a lebih besar sama dengan b: "+nilai1);
+        System.out.println("Nilai c lebih kecil sama dengan b: "+nilai2);
+    }
+}
