e8be17998dc0a49fd56e495687cb1125d5e9aef0
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-15 15:27:28 +0800

    Operator logika: AND

---
+++ Hari44.java
@@ -0,0 +1,15 @@
+package ISC100Hari;
+
+public class Day44 {
+    public static void main(String[] args) {
+        
+        int a = 4*4;
+        int b = 16;
+        
+        if((a%b == 0) && (a%2 == 0)){
+            System.out.println("Sisa bagi keduanya adalah 0");
+        }else{
+            System.out.println("Sisa bagi kedua atau salah satunya bukan 0");
+        }
+    }
+}
