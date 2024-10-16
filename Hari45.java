a8e7533472ac469f9dd1f28fa57323bea5d43b45
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-16 18:50:25 +0800

    Operator logika: OR

---
+++ Hari45.java
@@ -0,0 +1,14 @@
+package ISC100Hari;
+
+public class Day45 {
+    public static void main(String[] args) {
+        
+        boolean nilai;
+        
+        nilai = (5 > 2) || (5 == 2);
+        System.out.println("Apakah bernilai true atau false = "+nilai);
+        
+        nilai = (5 > 2) && (5 == 2);
+        System.out.println("Apakah bernilai true atau false = "+nilai);
+    }
+}
