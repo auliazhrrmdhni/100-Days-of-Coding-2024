d49aa03cc1f94e644064428cf0882116794ab556
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-17 17:59:55 +0800

    Operator logika: Negasi/kebalikan

---
+++ Hari46.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day46 {
+    public static void main(String[] args) {
+        
+        boolean cerah = false;
+        
+        System.out.println("Hari ini cuacanya cerah? "+!cerah);
+        System.out.println("Hari ini cuacanya cerah? "+cerah);
+    }
+}
