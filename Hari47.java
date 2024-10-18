349cba9dd7f649bc17f189fa6eb6f8a26b966154
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-18 21:19:31 +0800

    Percabangan: if

---
+++ Hari47.java
@@ -0,0 +1,13 @@
+package ISC100Hari;
+
+public class Day47 {
+    public static void main(String[] args) {
+        
+        int a = 4;
+        int b = 24;
+        
+        if(a%2==0 && b%2==0){
+            System.out.printf("Sisa bagi dari %d dan %d ketika dibagi dengan 2 adalah 0%n", a, b);
+        }
+    }
+}
