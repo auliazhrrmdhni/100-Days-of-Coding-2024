d2d4d94c777e91818e27e4e25a7cbbaef8df156f
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-20 23:01:57 +0800

    Percabangan: if- else if

---
+++ Day49.java
@@ -0,0 +1,22 @@
+package ISC100Hari;
+
+import java.util.Scanner;
+
+public class Day49 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nilai1: ");
+        int nilai1 = scanner.nextInt();
+        
+        System.out.print("Nilai2: ");
+        int nilai2 = scanner.nextInt();
+        
+        if(nilai1 > nilai2){
+            System.out.println("Nilai1 lebih besar dari nilai2");
+        }else if(nilai2 > nilai1){
+            System.out.println("Nilai2 lebih besar dari nilai1");
+        }
+    }
+}
