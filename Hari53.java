317b824ae29307a8f8354492d05eac8888b6cd8a
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-24 21:20:48 +0800

    Looping: for

---
+++ Hari53.java
@@ -0,0 +1,10 @@
+package ISC100Hari;
+
+public class Day53 {
+    public static void main(String[] args) {
+        
+        for(int i = 0; i <= 10; i++){
+            System.out.println("Hari ini cuaca panas!");
+        }
+    }
+}
