3a2587d7675e13315f85921c88353ccd5cd6fe18
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-25 20:38:57 +0800

    Looping: for increment

---
+++ Hari54.java
@@ -0,0 +1,10 @@
+package ISC100Hari;
+
+public class Day54 {
+    public static void main(String[] args) {
+        
+        for(int i= 1; i < 5; i++){
+            System.out.println(i);
+        }
+    }
+}
