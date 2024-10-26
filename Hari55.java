a6c96803a561a3d4c2411bcfc7db5265fd838f82
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-26 11:29:03 +0000

    Looping: for decrement

---
+++ Hari55.java
@@ -0,0 +1,10 @@
+package ISC100Hari;
+
+public class Day55 {
+    public static void main(String[] args) {
+        
+        for(int i = 10; i > 2; i--){
+            System.out.println(i);
+        }
+    }
+}
