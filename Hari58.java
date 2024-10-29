bdc8f3777189575fd36bd512675e003fb3bcde6c
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-29 19:50:53 +0800

    Looping: while decrement

---
+++ Hari58.java
@@ -0,0 +1,12 @@
+package ISC100Hari;
+
+public class Day58 {
+    public static void main(String[] args) {
+        
+        int i = 10;
+        while(i > 2){
+            System.out.println("Langitnya cantik");
+            i--;
+        }
+    }
+}
