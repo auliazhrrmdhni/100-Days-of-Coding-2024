08dcc2e2e7adde45576f5a3acf1ecd7b979f6ea4
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-10-30 20:48:58 +0800

    Looping For Each

---
+++ Hari59.java
@@ -0,0 +1,12 @@
+package ISC100Hari;
+
+public class Day59 {
+    public static void main(String[] args) {
+        
+        String[] hewan = {"Kucing", "Angsa", "Beruang", "Cicak", "Panda"};
+        
+        for(String binatang: hewan){
+            System.out.println(binatang);
+        }
+    }
+}
