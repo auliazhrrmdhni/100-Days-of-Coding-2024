b932ec212e36cd6f0ee547927781efbc215258e8
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-01 18:44:08 +0800

    Infinite Loop

---
+++ Hari61.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day61 {
+    public static void main(String[] args) {
+        
+        do{
+            System.out.println("Hii");
+        }
+        while(true);
+    }
+}
