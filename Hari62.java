1cf82ed633f30e33032a5efd9ca9ee3a0ea57297
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-02 18:58:55 +0800

    Keyword: break

--- Hari62.java
+++ Hari62.java
@@ -2,7 +2,7 @@ package ISC100Hari;
 
 public class Day62 {
     public static void main(String[] args) {
-        
+    
         for(int i = 0; i < 10; i++){
             if(i == 3){
                 break;
