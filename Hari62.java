73f161c33146a4a69a666841babac0f0549319c9
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-02 19:11:20 +0800

    Keyword: break

--- Hari62.java
+++ Hari62.java
@@ -5,7 +5,7 @@ public class Day62 {
         
         for (int i = 0; i < 10; i++) {
             if (i == 4) {
-                break;  // Keluar dari loop jika i sama dengan 5
+                break; 
             }
             System.out.println(i);
         }
