9bc96af1a5056c392a0fd28837e0d8e004f47496
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-02 18:58:08 +0800

    Create Hari62.java

---
+++ Hari62.java
@@ -0,0 +1,12 @@
+package ISC100Hari;
+
+public class Day62 {
+    public static void main(String[] args) {
+        
+        for(int i = 0; i < 10; i++){
+            if(i == 3){
+                break;
+            }
+        }
+    }
+}
