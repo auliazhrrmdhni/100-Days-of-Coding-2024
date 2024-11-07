0729ccb22a8674990e0c5c105ca507fb9b68d26a
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-07 19:15:05 +0800

    Method void

---
+++ Hari67.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day67 {
+    static void cuacaBuruk(){
+        System.out.println("Tadi hujan beserta petir");
+    }
+    
+    public static void main(String[] args) {
+        cuacaBuruk();
+    }
+}
