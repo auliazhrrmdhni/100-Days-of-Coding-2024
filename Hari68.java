58b4c3b79ce1562a38266585c7b4d64149bcb918
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-08 21:27:47 +0800

    Method Non-void

---
+++ Hari68.java
@@ -0,0 +1,12 @@
+package ISC100Hari;
+
+public class Day68 {
+   public static int persegiPanjang(){
+       return 3*5;
+   } 
+   
+    public static void main(String[] args) {
+        int luasPersegiPanjang = Day68.persegiPanjang();
+        System.out.println("Luas persegi panjang: "+luasPersegiPanjang);
+    }
+}
