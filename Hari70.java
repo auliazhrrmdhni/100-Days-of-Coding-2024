7c1398dc55f7dbedaef324b77c8308cd362123dc
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-10 21:03:27 +0800

    Method parameter

---
+++ Hari70.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day70 {
+    static void hariIni(String hari) {
+    System.out.println("Hari: "+hari);
+  }
+
+  public static void main(String[] args) {
+    hariIni("Minggu");
+  }
+}  jelaskan kode program itu!
