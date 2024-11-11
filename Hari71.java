8309e602a69801ff55b4b3195975e6025305207b
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-11 20:39:36 +0800

    Method Statitc

---
+++ Hari71.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day71 {
+    static int perkalian(int angka1, int angka2){
+        return angka1*angka2;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hasil perkalian: "+ Day71.perkalian(5, 3));
+    }
+}
