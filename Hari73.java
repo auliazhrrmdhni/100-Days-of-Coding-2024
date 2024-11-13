1cf54738a55523170e05ade72d909c5a610b1ab2
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-13 21:36:59 +0800

    String method: charAt()

---
+++ Hari73.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+public class Day73 {
+    public static void main(String[] args) {
+        
+        String kata = "Selamat malam!";
+        
+        System.out.println(kata.charAt(0));
+        System.out.println(kata.charAt(8));
+    }
+}
