bdc3405453ae5cae1690a4ca9f88a9d4a20f5a95
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-14 20:55:35 +0800

    String method: length

---
+++ Hari74.java
@@ -0,0 +1,15 @@
+package ISC100Hari;
+
+import java.util.Scanner;
+
+public class Day74 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan kalimat: ");
+        String kalimat = scanner.nextLine();
+        
+        System.out.println(kalimat+ " memiliki panjang "+ kalimat.length());
+    }
+}
