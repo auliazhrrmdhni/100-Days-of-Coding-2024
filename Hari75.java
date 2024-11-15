1c6636b0f36706d3931fcbcd66a926320a390f1d
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-15 21:32:09 +0800

    String Method: toLowerCase()

---
+++ Hari75.java
@@ -0,0 +1,9 @@
+package ISC100Hari;
+
+public class Day75 {
+    public static void main(String[] args) {
+        
+        String kalimat = "SELAMAT MALAM~";
+        System.out.println(kalimat.toLowerCase());
+    }
+}
