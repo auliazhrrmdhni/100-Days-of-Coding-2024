a0d057f66ee2d33ba222aa0285618cf846f90b2e
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-16 19:17:30 +0800

    String Method: toUpperCase()

---
+++ Hari76.java
@@ -0,0 +1,9 @@
+package ISC100Hari;
+
+public class Day76 {
+    public static void main(String[] args) {
+        
+        String kalimat = "cuaca hari ini panas";
+        System.out.println(kalimat.toUpperCase());
+    }
+}
