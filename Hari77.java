e243c382dbb7993ce220dea544b14dcd01fde680
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-17 18:46:29 +0800

    String Method: equals()

---
+++ Hari77.java
@@ -0,0 +1,13 @@
+package ISC100Hari;
+
+public class Day77 {
+    public static void main(String[] args) {
+        
+        String kata1 = "Selamat Malam";
+        String kata2 = "selamat malam";
+        String kata3 = "Selamat Malam";
+        
+        System.out.println(kata1.equals(kata2));
+        System.out.println(kata1.equals(kata3));
+    }
+}
