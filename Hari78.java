a49b05eba359f4541706626d2c42e8223a761cfd
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-18 20:03:41 +0800

    String method: equalsIgnoreCase()

---
+++ Hari78.java
@@ -0,0 +1,13 @@
+package ISC100Hari;
+
+public class Day78 {
+    public static void main(String[] args) {
+        
+        String kata1 = "Hello";
+        String kata2 = "hello";
+        String kata3 = "Malam";
+        
+        System.out.println(kata1.equalsIgnoreCase(kata2));
+        System.out.println(kata2.equalsIgnoreCase(kata3));
+    }
+}
