5ed4c9b4e8f27cd8cf53a9d971d9bf8dc322d94b
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-19 22:23:13 +0800

    String Method: toString()

---
+++ Hari79.java
@@ -0,0 +1,10 @@
+package ISC100Hari;
+
+public class Day79 {
+    public static void main(String[] args) {
+        
+        Integer angka = 23;
+        
+        System.out.println(angka.toString());
+    }
+}
