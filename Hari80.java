3174ddeaf78d4f2aa96d1a74864dbdf1489d0973
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-20 18:35:07 +0800

    String method: trim()

---
+++ Hari80.java
@@ -0,0 +1,9 @@
+package ISC100Hari;
+
+public class Day80 {
+    public static void main(String[] args) {
+        
+        String kalimat = "   Ini sebuah kalimat   ";
+        System.out.println(kalimat.trim());
+    }
+}
