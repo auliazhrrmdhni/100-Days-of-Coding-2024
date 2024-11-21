0cdabd8a66fe474f856e9b0ef6b93105c23663a6
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-21 19:16:27 +0800

    String method: valueOf()

---
+++ Hari81.java
@@ -0,0 +1,10 @@
+package ISC100Hari;
+
+public class Day81 {
+    public static void main(String[] args) {
+        
+        int angka = 2304;
+        String a = String.valueOf(angka);
+        System.out.println(a);
+    }
+}
