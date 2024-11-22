0c1d76f34f979540f5405811859d2e58ba7f7bf2
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-22 17:14:10 +0800

    Method ceil(), floor(), dan round()

---
+++ hari82.java
@@ -0,0 +1,18 @@
+package ISC100Hari;
+
+public class Day82 {
+    public static void main(String[] args) {
+        
+        double a = 2.3;
+        double c = 4.5;
+        
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.ceil(c));
+        
+        System.out.println(Math.floor(a));
+        System.out.println(Math.floor(c));
+        
+        System.out.println(Math.round(a));
+        System.out.println(Math.round(c));
+    }
+}
