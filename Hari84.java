06f0ded3bdf3369056b65a66c7846a73b510381d
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-25 19:06:47 +0800

    Mengisi elemen ke array satu dimensi pada saat diinisialisasi

---
+++ Hari84.java
@@ -0,0 +1,11 @@
+package ISC100Hari;
+
+import java.util.Arrays;
+
+public class Day84 {
+    public static void main(String[] args) {
+        
+        int angka[] = {23, 4, 14, 2, 28};
+        System.out.println(Arrays.toString(angka));
+    }
+}
