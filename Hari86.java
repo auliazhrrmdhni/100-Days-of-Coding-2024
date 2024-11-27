96383125246ca035223025447dad9e805e2937b9
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2024-11-27 20:04:37 +0800

    Mengubah nilai array satu dimensi pada indeks tertentu

---
+++ Hari86.java
@@ -0,0 +1,14 @@
+package ISC100Hari;
+
+import java.util.Arrays;
+
+public class Day86 {
+    public static void main(String[] args) {
+        
+        int genap[] = {24, 28, 4, 3};
+        System.out.println(Arrays.toString(genap));
+        
+        genap[3] = 6;
+        System.out.println(Arrays.toString(genap));
+    }
+}
